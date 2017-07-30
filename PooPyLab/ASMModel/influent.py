b44bc87eca5017a0aad07910d4f795970a26dad0
Kai Zhang <kaiz@KaiMBPLab.local>
2017-07-30 10:46:22 -0500

    influent class definition

--- PooPyLab/ASMModel/influent.py
+++ PooPyLab/ASMModel/influent.py
@@ -3,7 +3,7 @@
 # PooPyLab is a simulation software for biological wastewater treatment
 # processes using International Water Association Activated Sludge Models.
 #    
-#    Copyright (C) 2014  Kai Zhang
+#    Copyright (C) Kai Zhang
 #
 #    PooPyLab is free software: you can redistribute it and/or modify
 #    it under the terms of the GNU General Public License as published by
@@ -22,15 +22,16 @@
 #    Definition of the plant Influent unit.
 #
 # Change Log:
-#   June 16, 2015 KZ: Removed _PreFix, _Group status and 
+#   July 31, 2017 KZ: Made it more pythonic and changed to python3.
+#   June 16, 2015 KZ: Removed _prefix, _group status and 
 #                       Set(Get)PreFixStatus(), Set(Get)GroupStatus;
-#                       Renamed _Done to _Visited, SetAs(Is)Done() to
+#                       Renamed _Done to _visited, SetAs(Is)Done() to
 #                       SetAs(Is)Visited().
-#   March 20, 2015 KZ: Added _PreFix, _Group, _Done status and 
+#   March 20, 2015 KZ: Added _prefix, _group, _Done status and 
 #                       Set(Get)PreFixStatus(), Set(Get)GroupStatus, 
 #                       SetAs(Is)Done().
 #   November 18, 2014 KZ: Added UpstreamConnected() and set to True
-#   November 12, 2014 KZ: added _MainOutletConnected flag 
+#   November 12, 2014 KZ: added _main_outlet_connected flag 
 #                           and MainOutletConnected() function
 #   October 19, 2014 KZ: removed test code
 #   March 15, 2014: KZ: redefined for the new class structure.
@@ -39,7 +40,7 @@
 
 import base, constants
 
-class Influent(base.Base):
+class influent(base.base):
     __id = 0
 
     def __init__(self):
@@ -49,105 +50,110 @@ class Influent(base.Base):
         # Store the influent characteristics in a list()
         # For ASM #1:
         #
-        #    self._InfComp[0]: X_I
-        #    self._InfComp[1]: X_S
-        #    self._InfComp[2]: X_BH
-        #    self._InfComp[3]: X_BA
-        #    self._InfComp[4]: X_D
-        #    self._InfComp[5]: S_I
-        #    self._InfComp[6]: S_S
-        #    self._InfComp[7]: S_DO
-        #    self._InfComp[8]: S_NO
-        #    self._InfComp[9]: S_NH
-        #    self._InfComp[10]: S_NS
-        #    self._InfComp[11]: X_NS
-        #    self._InfComp[12]: S_ALK
-        self._InfComp = [0.0] * constants._NUM_ASM1_COMPONENTS
+        #    self._inf_comp[0]: X_I
+        #    self._inf_comp[1]: X_S
+        #    self._inf_comp[2]: X_BH
+        #    self._inf_comp[3]: X_BA
+        #    self._inf_comp[4]: X_D
+        #    self._inf_comp[5]: S_I
+        #    self._inf_comp[6]: S_S
+        #    self._inf_comp[7]: S_DO
+        #    self._inf_comp[8]: S_NO
+        #    self._inf_comp[9]: S_NH
+        #    self._inf_comp[10]: S_NS
+        #    self._inf_comp[11]: X_NS
+        #    self._inf_comp[12]: S_ALK
+        self._inf_comp = [0.0] * constants._NUM_ASM1_COMPONENTS
 
         # Influent characteristics from user measurements/inputs
         # Setting default values for municipal wastewater in USA
-        # TODO: July 10, 2013, Default Unit: mg/L except where noted differently
+        # Default Unit: mg/L except where noted differently
         self._BOD5 = 250.0
         self._TSS = 250.0
         self._VSS = 200.0
         self._TNK = 40.0
         self._NH3 = 28.0
-        self._NO = 0.0 #JULY 10, 2013
+        self._NO = 0.0
         self._TP = 10.0
-        self._Alk = 6.0 #in mmol/L as CaCO3
+        self._Alk = 6.0  # in mmol/L as CaCO3
         self._DO = 0.0 
 
-        # Plant influent flow in M3/DAY, JULY 10, 2013
-        # TODO: will be user-input from GUI, JULY 10, 2013. FROM THE GUI, USER
+        # Plant influent flow in M3/DAY
+        # TODO: will be user-input from GUI. FROM THE GUI, USER
         #   will use MGD. DEFAULT VALUE = 10 MGD. 
-        self._DesignFlow = 10.0 * 1000.0 / 3.78 #convert to M3/day, July 10, 2013
+        self._design_flow = 10 * 1000 / 3.78  # convert to M3/day
 
-        # Store the unit that receives the plant influent
-        self._Outlet = None
+        # the reactor that receives the plant influent
+        self._outlet = None
         
-        # _MainOutletConnected is to store the status about whether there is a
-        #   downstream unit.
-        self._MainOutletConnected = False
+        # the status about whether there is a downstream unit
+        self._main_outlet_connected = False
 
-        # _PreFix is True/False on whether the unit is already visited by the loop
-        #   finding process
-        self._PreFix = False
+        # boolean on whether the unit is already visited by the loop
+        # finding process
+        self._prefix = False
 
-        # _Group is '' (empty string) if the unit has not been assigned to any 
+        # _group is '' (empty string) if the unit has not been assigned to any
         #   specific group by the loop finding process.
-        # If the unit has been assigned to a group, _Group will record the group ID.
-        self._Group = ''
+        # If the unit is assigned to a group, _group will record the group ID.
+        #TODO: IS THIS STILL USED AT ALL?
+        self._group = ''
 
-        # _Visited is True/False on whether the loop finding process has finished
+        # boolean on whether the loop finding process has finished
         #   analyzing the unit
-        self._Visited = False
+        self._visited = False
         
         print self.__name__,' initialized successfully.'
 
-    def GetUpstreamUnits(self):
-        ''' Get the dict that stores all the upstream units that feed into current one
+    def get_upstream_units(self):
+        ''' Get the {} that stores all the upstream units that feed into
+            the current one
             Return Type: None (for Influent) or dict (for others)
         '''
         return None
 
-    def SetDownstreamMainUnit(self, SingleReceiver):
-        ''' Set the downstream unit that will receive effluent from the current unit'''
-        if self._Outlet != SingleReceiver: #if the specified SingleReceiver has not been added
-            self._Outlet = SingleReceiver 
-            self._MainOutletConnected = True
-            if SingleReceiver != None:
-                SingleReceiver.AddUpstreamUnit(self)
+    def set_downstream_main_unit(self, rcvr):
+        ''' Set the downstream unit that will receive effluent from 
+            the current unit
+        '''
+        if self._outlet != rcvr: #if the specified rcvr has not been added
+            self._outlet = rcvr 
+            self._main_outlet_connected = True
+            if rcvr != None:
+                rcvr.add_upstream_unit(self)
     
-    def UpstreamConnected(self):
-        '''Placeholder, always return True for Influent as it doesn't need an upstream unit'''
+    def upstream_connected(self):
+        '''Placeholder, always return True for Influent as it 
+            doesn't need an upstream unit
+        '''
         return True
     
-    def MainOutletConnected(self):
+    def main_outlet_connected(self):
         ''' Return the status of outlet connection'''
-        return self._MainOutletConnected
+        return self._main_outlet_connected
 
-    def GetDownstreamMainUnit(self):
+    def get_downstream_main_unit(self):
         ''' Get the single unit downstream of the current one
             Return Type: base.Base
         '''
-        return self._Outlet
+        return self._outlet
 
-    def SetAsVisited(self, Status = False):
-        self._Visited = Status
+    def set_as_visited(self, status=False):
+        self._visited = status
 
-    def IsVisited(self):
-        return self._Visited
+    def is_visited(self):
+        return self._visited
     
-    def TotalizeFlow(self):
+    def totalize_flow(self):
         ''' Totalize all the flows entering the current unit.
             Return type: NO Return
             Does nothing for Influent object as the flow is fixed.
         '''
         pass
 
-    def BlendComponents(self):
+    def blend_components(self):
         '''
-            BlendComponents() for Base mixes the contents in all inlet
+            blend_components() for Base mixes the contents in all inlet
             components and send to the OUTLET, assuming no reaction
             takes palce.
             The definition is changed in ASMReactor where the mixture
@@ -157,80 +163,81 @@ class Influent(base.Base):
         '''
         pass
     
-    def UpdateCombinedInput(self):
+    def update_combined_input(self):
         ''' Combined the flows and loads into the current unit
             Doesn't need to do anything since Influent object does not 
             receive any upstream input.
         '''
         pass
 
-    def GetOutletFlow(self):
+    def get_outlet_flow(self):
         ''' Return the total out flow of the current unit (mainstream)
-            Return value type: float/double
         '''
-        return self._DesignFlow
+        return self._design_flow
 
-    def GetOutletConcentrations(self):
+    def get_outlet_concs(self):
         ''' Return the effluent concentrations of the current unit (mainstream)
-            Return type: list
         '''
-        return self._InfComp
+        return self._inf_comp
 
-    def Discharge(self):
+    def discharge(self):
         ''' Pass the total flow and blended components to the next unit.
         '''
-        if self._Outlet != None:
-            self.GetDownstreamMainUnit().UpdateCombinedInput()
+        if self._outlet != None:
+            self.get_downstream_main_unit().update_combined_input()
     
-    def ReceiveFrom(self, SingleDischarger):
+    def receive_from(self, dschgr=None):
         ''' Influent doesn't need to receive anything'''
         pass
 
-    def Interpret(self):
-        ''' Convert user input parameters into the ones that the ASM model can understand '''
-         #TODO: July 10, 2013: the first set of conversion available here is for municipal 
+    def interpret(self):
+        ''' Convert user input parameters into the ones that the ASM model
+            can understand
+        '''
+        #TODO: the first set of conversion available here is for municipal 
         #   wastewater. Industrial wastewater may have completely different
         #   conversion factors and needs to be tested.
         
-        # JULY 10, 2013 ADDED
-        #influent biodegradable COD, BOD/COD = 1.71 for typ. muni.WW
+        # influent biodegradable COD, BOD/COD = 1.71 for typ. muni.WW
         Inf_CODb = self._BOD5 * 1.71
-        #influent total COD, COD/BOD5 = 2.04 per BioWin
+        # influent total COD, COD/BOD5 = 2.04 per BioWin
         Inf_CODt = self._BOD5 * 2.04
-        #influent total innert COD, 
+        # influent total innert COD, 
         Inf_CODi = Inf_CODt - Inf_CODb
-        #influent soluble innert COD
+        # influent soluble innert COD
         Inf_S_I = 0.13 * Inf_CODt
-        #influent particulate innert COD
+        # influent particulate innert COD
         Inf_X_I = Inf_CODi - Inf_S_I
-        #influent particulate biodegradable COD
+        # influent particulate biodegradable COD
         Inf_X_S = 1.6 * self._VSS - Inf_X_I
-        #influent soluble biodegradable COD
+        # influent soluble biodegradable COD
         Inf_S_S = Inf_CODb - Inf_X_S
-        #influent Heterotrophs (mgCOD/L), 
+        # influent Heterotrophs (mgCOD/L), 
         Inf_X_BH = 0.0
-        #influent Autotrophs (mgCOD/L), 
+        # influent Autotrophs (mgCOD/L), 
         Inf_X_BA = 0.0
-        #influent Biomass Debris (mgCOD/L), 
+        # influent Biomass Debris (mgCOD/L)
         Inf_X_D = 0.0
         
-        #influent TKN (mgN/L), NOT IN InfC
+        # influent TKN (mgN/L), NOT IN InfC
         Inf_TKN = self._TNK
-        #influent Ammonia-N (mgN/L), 
+        # influent Ammonia-N (mgN/L), 
         Inf_S_NH = self._NH3
-        #subdividing TKN into: 
-        # a) nonbiodegradable TKN 
-        NonBiodegradable_TKN_Ratio = 0.03 #TODO: need to be configurable
-        #NON-BIODEGRADABLE TKN WILL HAVE TO BE ADDED BACK TO THE EFFLUENT TN
+        # subdividing TKN into: 
+        #  a) nonbiodegradable TKN 
+        NonBiodegradable_TKN_Ratio = 0.03 # TODO: need to be configurable
+        # NON-BIODEGRADABLE TKN WILL HAVE TO BE ADDED BACK TO THE EFFLUENT TN
         Inf_nb_TKN = Inf_TKN * NonBiodegradable_TKN_Ratio
-        Soluble_Biodegradable_OrgN_Ratio = Inf_S_S / (Inf_S_S + Inf_X_S)#Grady et al '99
-        # b) soluble biodegrable TKN,     
-        Inf_S_NS = (Inf_TKN - Inf_S_NH - Inf_nb_TKN) * Soluble_Biodegradable_OrgN_Ratio
-        # c) particulate biodegradable TKN, 
-        Inf_X_NS = (Inf_TKN - Inf_S_NH - Inf_nb_TKN) * \
-                    (1.0 - Soluble_Biodegradable_OrgN_Ratio)
+        #  Grady 1999:
+        Soluble_Biodegradable_OrgN_Ratio = Inf_S_S / (Inf_S_S + Inf_X_S)
+        #  b) soluble biodegrable TKN,     
+        Inf_S_NS = (Inf_TKN - Inf_S_NH - Inf_nb_TKN)\
+                    * Soluble_Biodegradable_OrgN_Ratio
+        #  c) particulate biodegradable TKN
+        Inf_X_NS = (Inf_TKN - Inf_S_NH - Inf_nb_TKN)\
+                    * (1.0 - Soluble_Biodegradable_OrgN_Ratio)
         
-        #influent Nitrite + Nitrate (mgN/L), 
+        # influent Nitrite + Nitrate (mgN/L)
         Inf_S_NO = self._NO
         
         Inf_S_ALK = self._Alk
@@ -238,62 +245,62 @@ class Influent(base.Base):
         Inf_S_DO = self._DO
         
         #store the converted information in the ASM Components for the influent
-        self._InfComp = [Inf_X_I, Inf_X_S, Inf_X_BH, Inf_X_BA, Inf_X_D, \
+        self._inf_comp = [Inf_X_I, Inf_X_S, Inf_X_BH, Inf_X_BA, Inf_X_D, \
                             Inf_S_I, Inf_S_S, -Inf_S_DO, Inf_S_NO, Inf_S_NH, \
                             Inf_S_NS, Inf_X_NS, Inf_S_ALK]
         
-    def HasSidestream(self):
+    def has_sidestream(self):
         ''' Check if the current unit has a sidestream discharge.
             Default = False, i.e. no sidestream
             Return type: boolean
         '''
         return False
 
-    def SetFlow(self, Flow):
-        if Flow >= 0.0:
-            self._DesignFlow = Flow
-        return self._DesignFlow
+    def set_flow(self, flow):
+        if flow >= 0.0:
+            self._design_flow = flow
+        return self._design_flow
     
-    def GetTSS(self):
+    def get_TSS(self):
         ''' Return the Total Suspsended Solids (TSS) in the unit '''
         return self._TSS
 
-    def GetVSS(self):
+    def get_VSS(self):
         ''' Return the Volatile Suspended Solids (VSS) in the unit '''
         return self._VSS
 
-    def GetTotalCOD(self):
-        ''' Return the Total COD (both soluable and particulate) in the unit '''
-        return self._InfComp[0] + self._InfComp[1] + self._InfComp[2] \
-                + self._InfComp[3] + self._InfComp[4] + self._InfComp[5] \
-                + self._InfComp[6]
+    def get_total_COD(self):
+        ''' Return the Total COD (both soluable and particulate) in the unit'''
+        return self._inf_comp[0] + self._inf_comp[1] + self._inf_comp[2] \
+                + self._inf_comp[3] + self._inf_comp[4] + self._inf_comp[5] \
+                + self._inf_comp[6]
 
-    def GetSoluableCOD(self):
+    def get_soluble_COD(self):
         ''' Return the SOLUABLE COD in the unit '''
-        return self._InfComp[5] + self._InfComp[6]
+        return self._inf_comp[5] + self._inf_comp[6]
 
-    def GetParticulateCOD(self):
+    def get_particulate_COD(self):
         ''' Return the PARTICULATE COD in the unit '''
-        return self.GetTotalCOD() - self.GetSoluableCOD()
+        return self.get_total_COD() - self.get_soluble_COD()
     
-    def GetTN(self):
+    def get_TN(self):
         ''' Return the Total Nitrogen of the unit '''
-        return self._InfComp[8] + self._InfComp[9] \
-                + self._InfComp[10] + self._InfComp[11]
+        return self._inf_comp[8] + self._inf_comp[9] \
+                + self._inf_comp[10] + self._inf_comp[11]
 
-    def GetParticulateN(self):
+    def get_particulate_N(self):
         ''' Return organic nitrogen of the unit '''
-        return self._InfComp[11]
+        return self._inf_comp[11]
 
-    def GetSoluableN(self):
+    def get_soluble_N(self):
         ''' Return soluable nitrogen of the unit '''
-        return self.GetTN() - self.GetParticulateN()
+        return self.get_TN() - self.get_particulate_N()
 
-    def GetOrganicN(self):
+    def get_organic_N(self):
         ''' Return organic nitrogen of the unit '''
-        return self._InfComp[10] + self._InfComp[11]
+        return self._inf_comp[10] + self._inf_comp[11]
 
-    def GetInorganicN(self):
+    def get_inorganic_N(self):
         ''' Return inorganic nitrogen of the unit '''
-        return self._InfComp[8] + self._InfComp[9] 
+        return self._inf_comp[8] + self._inf_comp[9] 
 
