355e7ed63c8496119ea4a03d18788d245a7d2c52
Kai Zhang <kaiz@KaiMBPLab.local>
2017-07-30 18:25:48 -0500

    clarifier class definition

--- PooPyLab/ASMModel/clarifier.py
+++ PooPyLab/ASMModel/clarifier.py
@@ -3,7 +3,7 @@
 # PooPyLab is a simulation software for biological wastewater treatment
 # processes using International Water Association Activated Sludge Models.
 #    
-#    Copyright (C) 2014  Kai Zhang
+#    Copyright (C) Kai Zhang
 #
 #    PooPyLab is free software: you can redistribute it and/or modify
 #    it under the terms of the GNU General Public License as published by
@@ -23,17 +23,19 @@
 #    It is assumed that clarifiers are circular.
 #    Author: Kai Zhang
 #
-#    Change Log: 
-#        May 26, 2014 KZ: Changed base class from Base to Splitter
-#        July 11, 2013 KZ: Initial commit
+# Change Log: 
+#   July 30, 2017 KZ: made it more pythonic.
+#   March 21, 2017 KZ: Migrated to Python3
+#   May 26, 2014 KZ: Changed base class from Base to Splitter
+#   July 11, 2013 KZ: Initial commit
 #
 
 import splitter
 
-class Final_Clarifier(splitter.Splitter):
+class final_clarifier(splitter.splitter):
     __id = 0
     def __init__(self, ActiveVol=380.0, SWD=3.5, Temperature=20.0, DO=2.0):
-        splitter.Splitter.__init__(self)
+        splitter.splitter.__init__(self)
         self.__class__.__id += 1
         self.__name__ = "FinalClarifier_" + str(self.__id)
         # SWD = side water depth in meters, default = ~12 ft
@@ -41,6 +43,6 @@ class Final_Clarifier(splitter.Splitter):
         # Temperature = 20 C by default
         # DO = dissolved oxygen, default = 2.0 mg/L
 
-        self._ActiveVol = ActiveVol
+        self._active_vol = ActiveVol
         self._SWD = SWD
-        self._Area = self._ActiveVol / self._SWD
+        self._area = self._ActiveVol / self._SWD
