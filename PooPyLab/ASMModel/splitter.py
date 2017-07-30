62198b62265421228be159e1bb75be3e8c82e099
Kai Zhang <kaiz@KaiMBPLab.local>
2017-07-30 17:53:14 -0500

    splitter class definition

--- PooPyLab/ASMModel/splitter.py
+++ PooPyLab/ASMModel/splitter.py
@@ -20,123 +20,115 @@
 #
 #
 #
-#This file provides the definition of Splitter.
+# This file provides the definition of Splitter.
 #
-#Update Log:
-#   June 24, 2015 KZ: Updated SetDownstreamSideUnit() to differential main/side
-#   June 23, 2015 KZ: Rewrote sidestream to eliminate Branch class
-#   June 18, 2015 KZ: Removed _PreFix and _Group status and 
-#                       Set(Get)PreFixStatus(), Set(Get)GroupStatus;
-#                       Renamed _Done to _Visited and SetAs(Is)Done() to
-#                       SetAs(Is)Visited()
-#   March 20, 2015 KZ: Added _PreFix, _Group, _Done status and 
-#                       Set(Get)PreFixStatus(), Set(Get)GroupStatus, 
-#                       SetAs(Is)Done().
-#   November 18, 2014 KZ: renamed "SideStream" into "Sidestream";
-#                           Added _SidestreamConnected and SideOutletConnected()
-#   Sep 26, 2014 KZ: fixed pipe.Pipe.__init__
-#   April 27, 2014 KZ: Change the sidestream class from Influent() to Sidestream()
-#   April 18, 2014 KZ: Rewrite definition based on the new class system structure
-#   December 25, 2013 KZ: commented out the BlendComponent() function in ReceiveFrom()
-#   December 07, 2013
+# Update Log:
+# July 30, 2017 KZ: pythonic style
+# March 21, 2017 KZ: Migrated to Python3
+# June 24, 2015 KZ: Updated SetDownstreamSideUnit() to differential main/side
+# June 23, 2015 KZ: Rewrote sidestream to eliminate Branch class
+# June 18, 2015 KZ: Removed _PreFix and _Group status and 
+#                     Set(Get)PreFixStatus(), Set(Get)GroupStatus;
+#                     Renamed _Done to _Visited and SetAs(Is)Done() to
+#                     SetAs(Is)Visited()
+# March 20, 2015 KZ: Added _PreFix, _Group, _Done status and 
+#                     Set(Get)PreFixStatus(), Set(Get)GroupStatus, 
+#                     SetAs(Is)Done().
+# November 18, 2014 KZ: renamed "SideStream" into "Sidestream";
+#                         Added _SidestreamConnected and SideOutletConnected()
+# Sep 26, 2014 KZ: fixed pipe.Pipe.__init__
+# April 27, 2014 KZ: Change the sidestream class from Influent() to Sidestream()
+# April 18, 2014 KZ: Rewrite definition based on the new class system structure
+# December 25, 2013 KZ: commented out the BlendComponent() function in ReceiveFrom()
+# December 07, 2013
 #
 
 import pipe 
 
 
-class Splitter(pipe.Pipe):
+class splitter(pipe.pipe):
     __id = 0
 
     def __init__(self):
-        pipe.Pipe.__init__(self)
+        pipe.pipe.__init__(self)
         self.__class__.__id += 1
         self.__name__ = "Splitter_" + str(self.__id)
 
-        #self._Sidestream = None
-        #self.Sidestream.SetFlow(0.0)
-
-        #the main outlet is defined in pipe.Pipe as self._MainOutlet
-        # therefore add the _SideOutlet only here.
-        self._SideOutlet = None
+        # the main outlet is defined in pipe.pipe as _main_outlet
+        # therefore add the _side_outlet only here.
+        self._side_outlet= None
         
-        self._MainOutletFlow = 0.0
-        self._SideOutletFlow = 0.0
+        self._main_outlet_flow = 0.0
+        self._side_outlet_flow = 0.0
         
-        self._SideOutletConnected = False
+        self._side_outlet_connected = False
 
-        # _Visited is True/False on whether the loop finding process has finished
+        # boolean on whether the loop finding process has finished
         #   analyzing the unit
-        self._Visited = False
+        self._visited = False
        
-        self._SRTController = False
-        print self.__name__, "initialized successfully."
+        self._SRT_controller = False
+        print(self.__name__, "initialized successfully.")
     
-    def IsSRTController(self):
-        ''' Mark the Splitter whether it controls the plant's Solids Retention Time.
+    def is_SRT_controller(self):
+        ''' Mark the splitter whether it controls the plant's Solids Retention Time.
             Default value: False
         '''
-        return self._SRTController
+        return self._SRT_controller
 
-    def SetAsSRTController(self, setting = False):
+    def set_as_SRT_controller(self, setting=False):
         ''' Take user-input to set whether the current Splitter control plant's SRT'''
-        self._SRTController = setting
+        self._SRT_controller = setting
         #TODO: HOW DOES THIS IMPACT WAS FLOW BASED ON USER SPECIFIED SRT?
 
-    def SetSidestreamFlow(self, Flow):
-        self._SideOutletFlow = Flow
+    def set_sidestream_flow(self, flow):
+        self._side_outlet_flow = flow
         #TODO: Need to be able to dynamically update the sidestream flow
     
-    def TotalizeFlow(self):
-        ''' totalize the flow for the Splitter unit,
-        '''
-        self._TotalFlow = self._MainOutletFlow = 0.0
-        for unit in self._Inlet:
-            self._TotalFlow += self._Inlet[unit]
+    def totalize_flow(self):
+        ''' totalize the flow for the Splitter unit '''
+        self._total_flow = self._main_outlet_flow = 0.0
+        for unit in self._inlet:
+            self._total_flow += self._inlet[unit]
         #TODO: Need to pay close attention to the flow balance below during runtime
-        #self.Sidestream.TotalizeFlow()
-        #self._SideOutletFlow = self.Sidestream.GetOutletFlow()
-        self._MainOutletFlow = self._TotalFlow - self._SideOutletFlow
-        self._FlowTotalized = True
+        self._main_outlet_flow = self._total_flow - self._side_outlet_flow
+        self._flow_totalized = True
 
 
-    def SetDownstreamSideUnit(self, SingleReceiver):
+    def set_downstream_side_unit(self, rcvr):
         ''' Set the sidestream unit that will receive effluent from the current unit'''
-        if self._SideOutlet != SingleReceiver:
-            self._SideOutlet = SingleReceiver
-            self._SideOutletConnected = True
-            if SingleReceiver != None:
-                SingleReceiver.AddUpstreamUnit(self, "Side")#TODO: OKAY??
+        if self._side_outlet != rcvr:
+            self._side_outlet = rcvr
+            self._side_outlet_connected = True
+            if rcvr != None:
+                rcvr.add_upstream_unit(self, "Side")  #TODO: OKAY??
                 
-    def GetDownstreamSideUnit(self):
-        return self._SideOutlet
+    def get_downstream_side_unit(self):
+        return self._side_outlet
 
-    def Discharge(self):
+    def discharge(self):
         ''' Pass the total flow and blended components to the next unit.
             Both mainstream and sidestream units shall receive their flows and component
             concentratons.
         '''
-        self.UpdateCombinedInput()
-        if self._MainOutlet != None and self._Sidestream != None:
-            self.GetDownstreamMainUnit().UpdateCombinedInput()
-            self.GetDownstreamSideUnit().UpdateCombinedInput() 
+        self.update_combined_input()
+        if self._main_outlet != None and self.set_sidestream_flow != None:
+            self.get_downstream_main_unit().update_combined_input()
+            self.get_downstream_main_unit().update_combined_input() 
         else:
-            print "ERROR: ", self.__name__, " downstream unit setup not complete"
+            print("ERROR: ", self.__name__, " downstream unit setup not complete")
 
-    def HasSidestream(self):
-        ''' Check if the current unit has a sidestream discharge.
-            Default = False, i.e. no sidestream
-            Return type: boolean
-        '''
+    def has_sidestream(self):
         return True 
     
-    def SideOutletConnected(self):
-        return self._SideOutletConnected
+    def _side_outlet_connected(self):
+        return self._side_outlet_connected
 
-    def SetAsVisited(self, Status = False):
-        self._Visited = Status
+    def set_as_visited(self, status=False):
+        self._visited = status
 
-    def IsVisited(self):
-        return self._Visited
+    def is_visited(self):
+        return self._visited
  
     #def GetWAS(self, WWTP, TargetSRT): 
     #    '''Get the mass of DRY solids to be wasted (WAS) in KiloGram'''
@@ -149,6 +141,6 @@ class Splitter(pipe.Pipe):
     #    if TargetSRT > 0:
     #        return TotalSolids / TargetSRT
     #    else:
-    #        print "Error in Target SRT (equal to or less than 0 day; GetWAS() returns 0."
+    #        print("Error in Target SRT (equal to or less than 0 day; GetWAS() returns 0.")
     #        return 0;
 
