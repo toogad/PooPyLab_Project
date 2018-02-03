8190e6a57515cd1c57fb9d624fa34049077ada1b
Kai Zhang <kaiz@KaiMBPLab.local>
2018-02-03 11:43:06 -0600

    testing PFD partitioning

--- PooPyLab/ASMModel/PartitionTearing_Test_3.py
+++ PooPyLab/ASMModel/PartitionTearing_Test_3.py
@@ -23,18 +23,18 @@ Inf, Pipe, React, Splt, Eff = [], [], [], [], []
 
 
 for i in range(4):
-    Inf.append(influent.Influent())
-    Inf[i].SetFlow(10)#TODO: Units to be noted in ''' ''' if a function asks for params
-    Eff.append(effluent.Effluent())
+    Inf.append(influent.influent())
+    Inf[i].set_flow(10)#TODO: Units to be noted in ''' ''' if a function asks for params
+    Eff.append(effluent.effluent())
 
 for i in range(35):
-    Pipe.append(pipe.Pipe())
+    Pipe.append(pipe.pipe())
     
 for i in range(11):
-    Splt.append(splitter.Splitter())
+    Splt.append(splitter.splitter())
 
 for i in range(7):
-    React.append(reactor.ASMReactor())
+    React.append(reactor.asm_reactor())
 
 Splt[0].__name__ = 'C'
 Splt[1].__name__ = 'F'
@@ -48,7 +48,7 @@ Splt[8].__name__ = 'K'
 Splt[9].__name__ = 'I'
 Splt[10].__name__ = 'N'
 
-Clarifier = clarifier.Final_Clarifier()
+Clarifier = clarifier.final_clarifier()
 Clarifier.__name__ = "D"
 
 WAS1 = was.WAS()
@@ -62,132 +62,132 @@ React[5].__name__ = 'J'
 React[6].__name__ = 'P'
 
 
-Inf[0].SetDownstreamMainUnit(Pipe[0])
-Inf[0].SetFlow(40)
-Pipe[0].SetDownstreamMainUnit(React[2])
-React[2].SetDownstreamMainUnit(Pipe[1])
-Pipe[1].SetDownstreamMainUnit(React[0])
+Inf[0].set_downstream_main_unit(Pipe[0])
+Inf[0].set_flow(40)
+Pipe[0].set_downstream_main_unit(React[2])
+React[2].set_downstream_main_unit(Pipe[1])
+Pipe[1].set_downstream_main_unit(React[0])
 
-Inf[1].SetDownstreamMainUnit(Pipe[30])
-Pipe[30].SetDownstreamMainUnit(React[0])
-React[0].SetDownstreamMainUnit(Pipe[2])
-Pipe[2].SetDownstreamMainUnit(React[1])
-React[1].SetDownstreamMainUnit(Pipe[3])
-Pipe[3].SetDownstreamMainUnit(Splt[0])
+Inf[1].set_downstream_main_unit(Pipe[30])
+Pipe[30].set_downstream_main_unit(React[0])
+React[0].set_downstream_main_unit(Pipe[2])
+Pipe[2].set_downstream_main_unit(React[1])
+React[1].set_downstream_main_unit(Pipe[3])
+Pipe[3].set_downstream_main_unit(Splt[0])
 
-Splt[0].SetDownstreamMainUnit(Pipe[27])
-Splt[0].SetDownstreamSideUnit(Pipe[4])
-Splt[0].SetSidestreamFlow(1)
+Splt[0].set_downstream_main_unit(Pipe[27])
+Splt[0].set_downstream_side_unit(Pipe[4])
+Splt[0].set_sidestream_flow(1)
 
-Pipe[27].SetDownstreamMainUnit(Clarifier)
-Pipe[4].SetDownstreamMainUnit(Splt[1])
+Pipe[27].set_downstream_main_unit(Clarifier)
+Pipe[4].set_downstream_main_unit(Splt[1])
 
-Splt[1].SetDownstreamMainUnit(Pipe[5])
-Splt[1].SetDownstreamSideUnit(Pipe[24])
-Splt[1].SetSidestreamFlow(0.5)
+Splt[1].set_downstream_main_unit(Pipe[5])
+Splt[1].set_downstream_side_unit(Pipe[24])
+Splt[1].set_sidestream_flow(0.5)
 
-Pipe[24].SetDownstreamMainUnit(Splt[2])
-Pipe[5].SetDownstreamMainUnit(React[3])
+Pipe[24].set_downstream_main_unit(Splt[2])
+Pipe[5].set_downstream_main_unit(React[3])
 
-Splt[2].SetDownstreamMainUnit(Pipe[26])
-Splt[2].SetDownstreamSideUnit(Pipe[25])
-Splt[2].SetSidestreamFlow(0.1)
+Splt[2].set_downstream_main_unit(Pipe[26])
+Splt[2].set_downstream_side_unit(Pipe[25])
+Splt[2].set_sidestream_flow(0.1)
 
-Pipe[26].SetDownstreamMainUnit(Splt[0])
-Pipe[25].SetDownstreamMainUnit(Eff[3])
+Pipe[26].set_downstream_main_unit(Splt[0])
+Pipe[25].set_downstream_main_unit(Eff[3])
 
-Clarifier.SetDownstreamMainUnit(Pipe[29])
-Clarifier.SetDownstreamSideUnit(Pipe[28])
-Clarifier.SetSidestreamFlow(0.2)
+Clarifier.set_downstream_main_unit(Pipe[29])
+Clarifier.set_downstream_side_unit(Pipe[28])
+Clarifier.set_sidestream_flow(0.2)
 
-Pipe[29].SetDownstreamMainUnit(React[2])
-Pipe[28].SetDownstreamMainUnit(React[3])
+Pipe[29].set_downstream_main_unit(React[2])
+Pipe[28].set_downstream_main_unit(React[3])
 
-React[3].SetDownstreamMainUnit(Pipe[6])
+React[3].set_downstream_main_unit(Pipe[6])
 
-Pipe[6].SetDownstreamMainUnit(Splt[9])
+Pipe[6].set_downstream_main_unit(Splt[9])
 
-Splt[9].SetDownstreamMainUnit(Pipe[23])
-Splt[9].SetDownstreamSideUnit(Pipe[7])
-Splt[9].SetSidestreamFlow(0.02)
+Splt[9].set_downstream_main_unit(Pipe[23])
+Splt[9].set_downstream_side_unit(Pipe[7])
+Splt[9].set_sidestream_flow(0.02)
 
-Pipe[23].SetDownstreamMainUnit(React[4])
-Pipe[7].SetDownstreamMainUnit(React[5])
+Pipe[23].set_downstream_main_unit(React[4])
+Pipe[7].set_downstream_main_unit(React[5])
 
-React[5].SetDownstreamMainUnit(Pipe[8])
-React[4].SetDownstreamMainUnit(Pipe[13])
+React[5].set_downstream_main_unit(Pipe[8])
+React[4].set_downstream_main_unit(Pipe[13])
 
-Pipe[13].SetDownstreamMainUnit(Splt[3])
+Pipe[13].set_downstream_main_unit(Splt[3])
 
-Splt[3].SetDownstreamMainUnit(Pipe[14])
-Splt[3].SetDownstreamSideUnit(Pipe[15])
-Splt[3].SetSidestreamFlow(0.4)
+Splt[3].set_downstream_main_unit(Pipe[14])
+Splt[3].set_downstream_side_unit(Pipe[15])
+Splt[3].set_sidestream_flow(0.4)
 
-Pipe[14].SetDownstreamMainUnit(React[5])
-Pipe[15].SetDownstreamMainUnit(Eff[2])
+Pipe[14].set_downstream_main_unit(React[5])
+Pipe[15].set_downstream_main_unit(Eff[2])
 
-Pipe[8].SetDownstreamMainUnit(Splt[8])
+Pipe[8].set_downstream_main_unit(Splt[8])
 
-Splt[8].SetDownstreamMainUnit(Pipe[9])
-Splt[8].SetDownstreamSideUnit(Pipe[19])
-Splt[8].SetSidestreamFlow(0.35)
+Splt[8].set_downstream_main_unit(Pipe[9])
+Splt[8].set_downstream_side_unit(Pipe[19])
+Splt[8].set_sidestream_flow(0.35)
 
-Pipe[19].SetDownstreamMainUnit(Eff[1])
+Pipe[19].set_downstream_main_unit(Eff[1])
 
-Pipe[9].SetDownstreamMainUnit(Splt[6])
+Pipe[9].set_downstream_main_unit(Splt[6])
 
-Splt[6].SetDownstreamMainUnit(Pipe[31])
-Splt[6].SetDownstreamSideUnit(Pipe[10])
-Splt[6].SetSidestreamFlow(0.05)
+Splt[6].set_downstream_main_unit(Pipe[31])
+Splt[6].set_downstream_side_unit(Pipe[10])
+Splt[6].set_sidestream_flow(0.05)
 
-Pipe[31].SetDownstreamMainUnit(Splt[7])
-Pipe[10].SetDownstreamMainUnit(Splt[5])
+Pipe[31].set_downstream_main_unit(Splt[7])
+Pipe[10].set_downstream_main_unit(Splt[5])
 
-Splt[7].SetDownstreamMainUnit(Pipe[22])
-Splt[7].SetDownstreamSideUnit(Pipe[32])
-Splt[7].SetSidestreamFlow(0.002)
+Splt[7].set_downstream_main_unit(Pipe[22])
+Splt[7].set_downstream_side_unit(Pipe[32])
+Splt[7].set_sidestream_flow(0.002)
 
-Pipe[22].SetDownstreamMainUnit(Splt[4])
-Pipe[32].SetDownstreamMainUnit(Splt[10])
+Pipe[22].set_downstream_main_unit(Splt[4])
+Pipe[32].set_downstream_main_unit(Splt[10])
 
-Splt[10].SetDownstreamMainUnit(Pipe[33])
-Splt[10].SetDownstreamSideUnit(Pipe[34])
-Splt[10].SetSidestreamFlow(0.0001)
-Pipe[33].SetDownstreamMainUnit(Eff[0])
-Pipe[34].SetDownstreamMainUnit(Splt[6])
+Splt[10].set_downstream_main_unit(Pipe[33])
+Splt[10].set_downstream_side_unit(Pipe[34])
+Splt[10].set_sidestream_flow(0.0001)
+Pipe[33].set_downstream_main_unit(Eff[0])
+Pipe[34].set_downstream_main_unit(Splt[6])
 
-Splt[4].SetDownstreamMainUnit(Pipe[12])
-Splt[4].SetDownstreamSideUnit(Pipe[16])
-Splt[4].SetSidestreamFlow(0.03)
-Splt[4].SetAsSRTController(True)
+Splt[4].set_downstream_main_unit(Pipe[12])
+Splt[4].set_downstream_side_unit(Pipe[16])
+Splt[4].set_sidestream_flow(0.03)
+Splt[4].set_as_SRT_controller(True)
 
-Pipe[16].SetDownstreamMainUnit(WAS1)
-Pipe[12].SetDownstreamMainUnit(React[4])
+Pipe[16].set_downstream_main_unit(WAS1)
+Pipe[12].set_downstream_main_unit(React[4])
 
-Splt[5].SetDownstreamMainUnit(Pipe[17])
-Splt[5].SetDownstreamSideUnit(Pipe[11])
-Splt[5].SetSidestreamFlow(0.1)
-Pipe[11].SetDownstreamMainUnit(Splt[4])
-Pipe[17].SetDownstreamMainUnit(React[6])
+Splt[5].set_downstream_main_unit(Pipe[17])
+Splt[5].set_downstream_side_unit(Pipe[11])
+Splt[5].set_sidestream_flow(0.1)
+Pipe[11].set_downstream_main_unit(Splt[4])
+Pipe[17].set_downstream_main_unit(React[6])
 
-React[6].SetDownstreamMainUnit(Pipe[18])
-Pipe[18].SetDownstreamMainUnit(Splt[8])
+React[6].set_downstream_main_unit(Pipe[18])
+Pipe[18].set_downstream_main_unit(Splt[8])
 
-Inf[2].SetDownstreamMainUnit(Pipe[20])
-Inf[2].SetFlow(10)
-Pipe[20].SetDownstreamMainUnit(Splt[5])
+Inf[2].set_downstream_main_unit(Pipe[20])
+Inf[2].set_flow(10)
+Pipe[20].set_downstream_main_unit(Splt[5])
 
-Inf[3].SetDownstreamMainUnit(Pipe[21])
-Inf[3].SetFlow(20)
-Pipe[21].SetDownstreamMainUnit(React[6])
+Inf[3].set_downstream_main_unit(Pipe[21])
+Inf[3].set_flow(20)
+Pipe[21].set_downstream_main_unit(React[6])
 
 WWTP = Inf + Pipe + React + Splt + Eff
 WWTP.append(WAS1)
 WWTP.append(Clarifier)
 
-response = raw_input("continue?")
+response = input("continue?")
 
-print "Begin Connection Test..."
+print("Begin Connection Test...")
 
 CheckPlantConnection(WWTP)
 CheckUpstream(WWTP)
@@ -196,7 +196,19 @@ CheckDownstream(WWTP)
 Groups = FindGroups(WWTP)
 
 for sub in Groups:
-    print "[ ",
+    print("[ ", end=' ')
     for unit in sub:
-        print unit.__name__,";",
-    print "]"
+        print(unit.__name__,";", end=' ')
+    print("]")
+
+ElementalCircuits = []
+import pdb
+pdb.set_trace()
+FindLoops(Groups, ElementalCircuits)
+print(len(ElementalCircuits))
+for subgroup in ElementalCircuits:
+    print(len(subgroup))
+    print("[ ", end=' ')
+    for t in subgroup:
+        print(t.__name__, "; ", end=' ')
+    print("]")
