4fe79720fac650619cb061eed4a221257e2c2bcc
Kai Zhang <toogad@gmail.com>
2019-10-29 17:15:08 -0500

    passed the test for 4-stg Bardenpho process

---
+++ tests/FOUR_STG_BARDEN.py
@@ -0,0 +1,143 @@
+# This file is part of PooPyLab.
+#
+# PooPyLab is a simulation software for biological wastewater treatment
+# processes using International Water Association Activated Sludge Models.
+#    
+#    Copyright (C) Kai Zhang
+#
+#    PooPyLab is free software: you can redistribute it and/or modify
+#    it under the terms of the GNU General Public License as published by
+#    the Free Software Foundation, either version 3 of the License, or
+#    (at your option) any later version.
+#
+#    PooPyLab is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#    GNU General Public License for more details.
+#
+#    You should have received a copy of the GNU General Public License
+#    along with PooPyLab.  If not, see <http://www.gnu.org/licenses/>.
+#
+# -----------------------------------------------------------------------------
+#    Definition of a 4-stage Bardenpho Process Flow Diagram.
+#
+#
+#    Process Flow Diagram:
+#
+# inlet--p1->rPAX--p2->rPOX--p3-->splt1--p4->rAXP--p5->rOXP--p6->fc--p7->outlet
+#            ^ ^                   |                    |
+#            | |                   |                    p8
+#            | |                   v                    |
+#            | +-----IR(p11)-------+                    |
+#            |                                          V
+#            +<-------RAS(p9)----splt2*<----------------+
+#                                 |
+#                                 +------p10--> waste(WAS)
+#
+#
+#       splt*: SRT Controlling Splitter
+#       rPAX : reactor_Pre_Anoxic
+#       rPOX : reactor_Pre_Oxic
+#       rAXP : reactor_Post_Anoxic
+#       rOXP : reactor_Post_Oxic
+#
+#    Author: Kai Zhang
+#
+# Change Log: 
+#   2019-10-29 KZ: init
+#
+
+from unit_procs.streams import influent, effluent, WAS, splitter, pipe
+from unit_procs.bio import asm_reactor
+from unit_procs.physchem import final_clarifier
+from utils.pfd import check, show
+
+
+inlet = influent()
+
+p1 = pipe()
+p2 = pipe()
+p3 = pipe()
+p4 = pipe()
+p5 = pipe()
+p6 = pipe()
+p7 = pipe()
+p8 = pipe()
+# return activated sludge
+RAS = pipe()  # aka p9
+RAS.__name__ = 'RAS'
+p10 = pipe()
+# internal recirculation
+IR = pipe()  # aka p11
+IR.__name__ = 'IR'
+
+rPAX = asm_reactor()
+rPOX = asm_reactor()
+rAXP = asm_reactor()
+rOXP = asm_reactor()
+
+fc = final_clarifier()
+
+outlet = effluent()
+
+splt1 = splitter()
+splt2 = splitter()
+
+waste = WAS()
+
+
+wwtp = [inlet,
+        p1, p2, p3, p4, p5, p6, p7, p8,
+        RAS, p10, IR,
+        rPAX, rPOX, rAXP, rOXP, fc, 
+        outlet, waste, 
+        splt1, splt2]
+
+SRT = 15  # day
+
+def construct():
+    # make an 4-stage plant
+    inlet.set_downstream_main(p1)
+    p1.set_downstream_main(rPAX)
+    rPAX.set_downstream_main(p2)
+    p2.set_downstream_main(rPOX)
+    rPOX.set_downstream_main(p3)
+    p3.set_downstream_main(splt1)
+    splt1.set_downstream_main(p4)
+    splt1.set_downstream_side(IR)
+    p4.set_downstream_main(rAXP)
+    IR.set_downstream_main(rPAX)
+    rAXP.set_downstream_main(p5)
+    p5.set_downstream_main(rOXP)
+    rOXP.set_downstream_main(p6)
+    p6.set_downstream_main(fc)
+    fc.set_downstream_main(p7)
+    fc.set_downstream_side(p8)
+    p7.set_downstream_main(outlet)
+    p8.set_downstream_main(splt2)
+    splt2.set_downstream_main(RAS)
+    splt2.set_downstream_side(p10)
+    RAS.set_downstream_main(rPAX)
+    p10.set_downstream_main(waste)
+
+    splt2.set_as_SRT_controller(True)
+
+    inlet.set_mainstream_flow(37800)
+
+    splt1.set_sidestream_flow(3.0*37800)  #IR
+    splt2.set_mainstream_flow(0.5*37800)  #RAS
+    
+    rPAX.set_model_condition(10, 0.0)
+    rPOX.set_model_condition(10, 2.0)
+    rAXP.set_model_condition(10, 0.0)
+    rOXP.set_model_condition(10, 1.0)
+
+    rPAX.set_active_vol(3000)
+    rPOX.set_active_vol(9500)
+    rAXP.set_active_vol(1000)
+    rOXP.set_active_vol(500)
+
+    print("Four-stage Bardenpho PFD constructed.")
+
+    return wwtp
+ 
---
+++ tests/FOUR_STG_BARDEN_test.py
@@ -0,0 +1,136 @@
+#   This file is part of PooPyLab.
+#
+#    PooPyLab is a simulation software for biological wastewater treatment
+#    processes using the International Water Association Activated Sludge
+#    Models.
+#   
+#    Copyright (C) Kai Zhang
+#
+#    This program is free software: you can redistribute it and/or modify
+#    it under the terms of the GNU General Public License as published by
+#    the Free Software Foundation, either version 3 of the License, or
+#    (at your option) any later version.
+#
+#    This program is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#    GNU General Public License for more details.
+#
+#    You should have received a copy of the GNU General Public License
+#    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+#
+#
+# --------------------------------------------------------------------
+#    Testing the influent/effluent/pipe/reactor classes.
+#
+#
+# Change Log:
+# 20191029 KZ: init and passed
+#
+
+from unit_procs.streams import pipe, influent, effluent
+from unit_procs.bio import asm_reactor
+import utils.pfd
+import utils.run
+import pdb
+
+if __name__ == '__main__':
+
+    import FOUR_STG_BARDEN 
+
+    wwtp = FOUR_STG_BARDEN.construct()
+
+    utils.pfd.check(wwtp)
+
+    utils.pfd.show(wwtp)
+
+    # identify units of different types
+    _inf = utils.pfd.get_all_units(wwtp, 'Influent')
+
+    _reactors = utils.pfd.get_all_units(wwtp, 'ASMReactor')
+
+    # TODO: _WAS may be an empty []
+    _WAS = utils.pfd.get_all_units(wwtp, 'WAS')
+
+    _splitters = utils.pfd.get_all_units(wwtp, 'Splitter')
+
+    _srt_ctrl = [_u for _u in _splitters if _u.is_SRT_controller()]
+
+    _final_clar = utils.pfd.get_all_units(wwtp, 'Final_Clarifier')
+
+    _eff = utils.pfd.get_all_units(wwtp, 'Effluent')
+
+    print('Influent in the PFD: {}'.format([_u.__name__ for _u in _inf]))
+
+    _plant_inf_flow = sum([_u.get_main_outflow() for _u in _inf])
+    print(' Total influent flow into plant:', _plant_inf_flow)
+
+    print('Reactors in the PFD: {}'.format([_u.__name__ for _u in _reactors]))
+
+    print('WAS units in the PFD: {}'.format([_u.__name__ for _u in _WAS]))
+
+    print('Splitters in the PFD: {}'.format(
+        [_u.__name__ for _u in _splitters]))
+
+    print('SRT Controlling Splitter in the PFD: {}'.format(
+        [_u.__name__ for _u in _srt_ctrl]))
+
+    print('Final Clarifier in the PFD: {}'.format(
+        [_u.__name__ for _u in _final_clar]))
+
+    print('Effluent in the PFD: {}'.format([_u.__name__ for _u in _eff]))
+
+    # start the main loop
+    _WAS_flow = 0.0  # M3/d
+    _SRT = FOUR_STG_BARDEN.SRT
+
+    # get the influent ready
+    for _u in _inf:
+        _u.update_combined_input()
+        _u.discharge()
+
+    # TODO: what if there are multiple influent units?
+    _params = _reactors[0].get_model_params()
+    _seed = utils.run.initial_guess(_params, 
+                                    _reactors,
+                                    _inf[0].get_main_outflow(), 
+                                    _inf[0].get_main_outlet_concs())
+    
+    print('Initial guess = {}'.format(_seed))
+
+    for _r in wwtp:
+        _r.assign_initial_guess(_seed)
+
+    for fc in _final_clar:
+        fc.set_capture_rate(0.985)
+
+    
+    utils.run.forward_set_flow(wwtp)
+    _WAS_flow = _WAS[0].set_WAS_flow(_SRT, _reactors, _eff)
+    _WAS[0].set_mainstream_flow(_WAS_flow)
+    _eff[0].set_mainstream_flow(_plant_inf_flow - _WAS_flow)
+    utils.run.backward_set_flow([_WAS[0], _eff[0]])
+    #pdb.set_trace()
+    utils.run.traverse_plant(wwtp, _inf[0])
+
+    max = 6500
+    r = 1
+    while r <= max:
+        _WAS_flow = _WAS[0].set_WAS_flow(_SRT, _reactors, _eff)
+        _WAS[0].set_mainstream_flow(_WAS_flow)
+        _eff[0].set_mainstream_flow(_plant_inf_flow - _WAS_flow)
+        utils.run.backward_set_flow([_WAS[0], _eff[0]])
+        utils.run.traverse_plant(wwtp, _inf[0])
+
+        if utils.run.check_global_cnvg(wwtp) or r == max:
+            break
+        r += 1
+
+    utils.run.show_concs(wwtp)
+
+    print("TOTAL ITERATION = ", r)
+
+    for i in range(len(_reactors)):
+        print(_reactors[i].get_active_vol())
+        print(_reactors[i].get_model_params())
+        print(_reactors[i].get_model_stoichs())
