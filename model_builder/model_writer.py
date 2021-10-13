296c7c1e2ddc63981398408d374db5dd89deed97
kai <toogad@gmail.com>
2021-10-13 18:05:26 -0500

    ready to read model stored in .csv file and store its stoichiometry, rate equations, and components

--- model_builder/model_writer.py
+++ model_builder/model_writer.py
@@ -22,7 +22,7 @@
 #  Definition of the equation writing functions.
 #
 
-def set_model_components(all_rows):
+def get_model_components(all_rows):
     """ Initialize lists for model components and their names."""
 
     _num_comps = len([cell for cell in all_rows[0] if cell != ''])
@@ -35,7 +35,7 @@ def set_model_components(all_rows):
     return _comps
 
 
-def set_model_params(all_rows=[]):
+def get_model_params(all_rows=[]):
     """ Initialize the model parameters. """
     _params = {}
 
@@ -47,20 +47,20 @@ def set_model_params(all_rows=[]):
     return _params
 
 
-def set_model_stoichs(all_rows=[], num_comps=13):
+def get_model_stoichs(all_rows=[], num_comps=13):
     """ Initialize the stoichiometrics."""
 
     starting_row_index = 4
 
     # count the number of rate equations first
     row_counter = 0
-    while (all_rows[row_counter][0] != 'END_ST'):
+    while (not(all_rows[row_counter][0] == 'END_STOICH' or all_rows[row_counter][0] == 'END_EQTNS')):
+        # 'END_STOICH' is the same Stop sign as 'END_EQTNS' in the Peterson matrix format
         row_counter += 1
 
     num_eqs = row_counter - starting_row_index
 
-
-    # the _stoichs stores the stoichiometrics in the form of a 2-d array of text;
+    # _stoichs stores the stoichiometrics in the form of a 2-d array of text;
     # the 1st index of _stoichs is the id of the model component, starting at 0
     # and the 2nd index of _stoichs is the id of the rate equation, starting at 0
     _stoichs = []
@@ -73,6 +73,25 @@ def set_model_stoichs(all_rows=[], num_comps=13):
     return _stoichs, num_eqs
 
 
+def get_rate_equations(all_rows=[], num_comps=13, num_eqtns=8):
+    """ Construct the rate equations from their individual terms """
+
+    # The definition of rate equations starting at Row 5 (Row Index = 4), three (3) columns down from the last model
+    # component and its stoichiometric column
+    eqs_start_row_index = 4
+    eqs_start_col_index = (num_comps - 1) + 3  # convert to index
+
+    # _rate_eqs stores the rate equations in the form of a 2-d array of text;
+    # the 1st index of _rate_eqs is the id of the rate equations, starting at 0
+    # and the 2nd index of _rate_eqs is the id of the individual terms in the rate equations, starting at 0
+    _rate_eqs = []
+
+    for row in all_rows[eqs_start_row_index:eqs_start_row_index + num_eqtns]:
+        _rate_eqs.append(row[eqs_start_col_index:])
+
+    return _rate_eqs
+
+
 if __name__ == '__main__':
 
     import csv
@@ -83,37 +102,25 @@ if __name__ == '__main__':
         r = csv.reader(csvf)
         for row in r:
             csv_rows.append(row)
-            print(row)
     csvf.close()
 
     print()
 
-    model_comps = set_model_components(csv_rows)
+    model_comps = get_model_components(csv_rows)
     num_comps = len(model_comps)
 
     print('Found', num_comps, 'Model Components:')
     print(model_comps)
 
-    model_params = set_model_params(csv_rows)
+    model_params = get_model_params(csv_rows)
+    num_params = len(model_params)
+    print('Found', num_params, 'Model Parameters/Constants:')
     print(model_params)
 
-    model_stoichs, num_eqs = set_model_stoichs(csv_rows, num_comps)
-
-    print('Found', num_eqs, 'Rate Equations')
+    model_stoichs, num_eqs = get_model_stoichs(csv_rows, num_comps)
     print(model_stoichs)
 
+    print('Found', num_eqs, 'Rate Equations:')
+    model_rate_eqs = get_rate_equations(csv_rows, num_comps, num_eqs)
+    print(model_rate_eqs)
 
-
-#    with open("model.csv", 'r') as csvf:
-#        dr = csv.DictReader(csvf)
-#        for row in dr:
-#            print(row)
-#    csvf.close()
-#
-#    with open("model.csv", 'r') as csvf:
-#        dr = csv.DictReader(csvf)
-#        for row in dr:
-#            print(row.keys())
-#            print(row.values())
-#    csvf.close()
-#
