e1f52a68339a0849815571e7f037c546662c3efe
kai <toogad@gmail.com>
2022-03-12 19:26:03 -0600

    some random edits while testing the new plugins in nvim

--- setup.py
+++ setup.py
@@ -4,11 +4,11 @@ with open("README.md", "r") as fh:
     long_des = fh.read()
 
 setuptools.setup(
-    name='poopylab', 
-    version='0.01.0.20201206', 
+    name='poopylab',
+    version='0.01.0.20201206',
     author="Kai Zhang",
     author_email="poopylabproject@gmail.com",
-    description="The .#2. best simulation tool for biological wastewater treatment, by the poop nerds, for the the poop nerds.",
+    description="The .#2. best simulation tool for biological wastewater treatment, by the nerds, for the the nerds.",
     long_description=long_des,
     long_description_content_type="text/markdown",
     keywords="Activated Sludge Model ASM Wastewater Simulation",
@@ -16,16 +16,15 @@ setuptools.setup(
     project_urls={
         'Source': 'https://github.com/toogad/PooPyLab_Project',
         'Documentation': 'https://toogad.github.io/brownbook/index.html',
-        'Tracker': 'https://github.com/toogad/PooPyLab_Project/issues', 
+        'Tracker': 'https://github.com/toogad/PooPyLab_Project/issues',
         'Funding': 'https://donate.pypi.org'
-        },
+    },
     packages=setuptools.find_packages(include=['PooPyLab', 'PooPyLab.*']),
     classifiers=[
         "Programming Language :: Python :: 3",
         "License :: OSI Approved :: GNU General Public License v3 (GPLv3)",
         "Operating System :: OS Independent"
-        ],
+    ],
     python_requires='>=3',
     install_requires=['scipy']
-    )
-
+)
