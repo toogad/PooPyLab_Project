8b98ead21570fd4f242349ed15227733270ec04c
Kai Zhang <toogad@gmail.com>
2023-12-03 22:59:39 -0600

    trying a simple case of pipe

--- model_builder/sundials/model_composer.py
+++ model_builder/sundials/model_composer.py
@@ -1,4 +1,10 @@
 #!/usr/bin/python3
 
 if __name__ == '__main__':
-    with open('template.c', 'r') as tmplt_file:
+    lines = []
+    items = {}
+    with open('pipe.ppm', 'r') as def_file:
+        lines = def_file.readlines()
+        items = {it.split('=')[0]: it.split('=')[1][:-1] for it in lines}
+    print(lines)
+    print(items)
