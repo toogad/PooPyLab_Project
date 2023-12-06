ab17b4003750650064498540ef100c64adc5d9ea
Kai Zhang <toogad@gmail.com>
2023-12-05 22:28:28 -0600

    array name creator

--- model_builder/sundials/model_composer.py
+++ model_builder/sundials/model_composer.py
@@ -1,5 +1,11 @@
 #!/usr/bin/python3
 
+def _create_var_defs(template_items={}):
+    _vd = list(template_items.values())
+    array_name = [s+'_' for s in _vd[:-1]]
+    array_name.append('array['+template_items['NUM_MODEL_COMPONENTS']+']')
+    return array_name
+
 if __name__ == '__main__':
     lines = []
     items = {}
@@ -8,3 +14,4 @@ if __name__ == '__main__':
         items = {it.split('=')[0]: it.split('=')[1][:-1] for it in lines}
     print(lines)
     print(items)
+    print(_create_var_defs(items))
