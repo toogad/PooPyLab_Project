b14df3cf35be11653a5f2b02efb7f769fcd29098
toogad <toogad@gmail.com>
2023-12-06 20:09:33 -0600

    created array definition for one end of a pipe

--- model_builder/sundials/model_composer.py
+++ model_builder/sundials/model_composer.py
@@ -1,10 +1,15 @@
 #!/usr/bin/python3
 
-def _create_var_defs(template_items={}):
+def _create_var_defs(template_items={}, array_str='comp'):
     _vd = list(template_items.values())
-    array_name = [s+'_' for s in _vd[:-1]]
-    array_name.append('array['+template_items['NUM_MODEL_COMPONENTS']+']')
-    return array_name
+    array_name = '_'.join(_vd[:-1])
+    #template_items['NUM_MODEL_COMPONENTS'] excludes flow rate
+    #adding 1 to the array length so that array[0] = flow rate
+    #and the model components in the array jive with the human math
+    array_len = int(template_items['NUM_MODEL_COMPONENTS']) + 1
+    array_name += '_' + array_str + '[' + str(array_len) + ']'
+    return ''.join(array_name)
+
 
 if __name__ == '__main__':
     lines = []
@@ -12,6 +17,5 @@ if __name__ == '__main__':
     with open('pipe.ppm', 'r') as def_file:
         lines = def_file.readlines()
         items = {it.split('=')[0]: it.split('=')[1][:-1] for it in lines}
-    print(lines)
-    print(items)
-    print(_create_var_defs(items))
+    comp_array_def = "realtype " + _create_var_defs(items)
+    print(comp_array_def)
